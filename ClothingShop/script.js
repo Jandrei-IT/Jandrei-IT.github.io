05532c53a64d0b2a9f26a0c4eadc93ec6163b407
Jandrei-IT <drytzy@gmail.com>
2026-01-13 00:07:37 +0800

    Add files via upload

---
+++ ClothingShop/script.js
@@ -0,0 +1,4 @@
+const form = document.getElementById("contactForm");
+form.addEventListener("submit", function(event) { event.preventDefault(); 
+    const name = document.getElementById("name").value; const message = document.getElementById("message").value;
+    alert("Name: " + name + "\nMessage: " + message); });
